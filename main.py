b4e55d73e98d6c010bc674e27f261cfb3045744f
MrBinit <sapkotabinit2002@gmail.com>
2025-01-25 22:38:56 +0545

    explored basic about the dataset

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import torch
 
 df = pd.read_csv('/home/binit/network_intrusion_detection_system/CIDDS-001/traffic/OpenStack/CIDDS-001-internal-week1.csv')
 df.head(5)
-df.columns()
+
 df = df.drop(columns=['Src Pt', 'Dst Pt', 'Flows', 'Tos','class', 'attackID', 'attackDescription'])
 
 
@@ -25,3 +25,15 @@ df['Date first seen'] = pd.to_datetime(df['Date first seen'])
 count_labels = df['attackType'].value_counts() / len(df)* 100
 plt.pie(count_labels[:3], labels=df['attackType'].unique()[:3], autopct='%.0f%%')
 
+plt.savefig("/home/binit/network_intrusion_detection_system/count_label.png")
+
+
+fig, ((ax1, ax2, ax3)) = plt.subplots(1, 3,
+figsize=(20,5))
+df['Duration'].hist(ax=ax1)
+ax1.set_xlabel("Duration")
+df['Packets'].hist(ax=ax2)
+ax2.set_xlabel("Number of packets")
+pd.to_numeric(df['Bytes'], errors='coerce').hist(ax=ax3)
+ax3.set_xlabel("Number of bytes")
+plt.savefig("/home/binit/network_intrusion_detection_system/distribution.png")
